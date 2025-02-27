78b196b2a631c8a6a79de71fa5cc6f73cb28515a
Shlok Kaushik <54725967+ShlokKaushik23@users.noreply.github.com>
2025-02-28 01:43:18 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,7 +105,7 @@ export default function Page() {
           <div className="flex flex-wrap gap-1">
             {DATA.skills.map((skill, id) => (
               <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
-                <Badge key={skill}>{skill}</Badge>
+{/*                 <Badge key={skill}>{skill}</Badge> */}
               </BlurFade>
             ))}
           </div>
