c57eafca41107a34fdf90281a7fd89cc715dc573
Shlok Kaushik <shlokkaushik23@gmail.com>
2025-04-04 00:22:19 +0530

    remove Links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -137,15 +137,15 @@ export default function Page() {
                 delay={BLUR_FADE_DELAY * 12 + id * 0.05}
               >
                 <ProjectCard
-                  href={project.href}
+                  // href={project.href}
                   key={project.title}
                   title={project.title}
                   description={project.description}
                   dates={project.dates}
                   tags={project.technologies}
                   image={project.image}
-                  video={project.video}
-                  links={project.links}
+                  // video={project.video}
+                  // links={project.links}
                 />
               </BlurFade>
             ))}
